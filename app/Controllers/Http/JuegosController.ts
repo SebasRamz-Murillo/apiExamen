5d40b5b4c471a588416392c909ab083c7fa22eae
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-03-28 18:47:43 -0600

    ws de monitores

--- app/Controllers/Http/JuegosController.ts
+++ app/Controllers/Http/JuegosController.ts
@@ -1,9 +1,11 @@
+import { Response } from '@adonisjs/core/build/standalone';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 //import Ws from 'App/Services/Ws'
 import Database from '@ioc:Adonis/Lucid/Database'
+import Ws from 'App/Services/Ws'
 
 export default class JuegosController {
-  public async monitoresDisponibles ({ }) {
+  public async monitoresDisponibles ({response }) {
     const _ = require('lodash');
     const monitoresTotal = [1,2,3,4,5,6,7,8,9,10];
     const monitoresUsers = await (await Database
@@ -11,23 +13,20 @@ export default class JuegosController {
       .select('monitor'))
       .map(row => row.monitor);
     const monitoresFaltantes = _.difference(monitoresTotal, monitoresUsers);
-    return monitoresFaltantes;
+    Ws.io.emit('monitores', monitoresFaltantes);
+    response.send(monitoresFaltantes)
   }
 
   //juego de un barquito, dependiendo de el monitor, es el orden en que se va a ir viendo el barco(tres numeros:1 para barco entero, 2 para la mitad izquierda, 3 para la mitad derecha) con websocker
   public async barquito ({ request, response, auth }) {
 
     const monitor = request.input('monitor')
-    const user = await auth.authenticate()
-    const monitoresFaltantes = await this.monitoresDisponibles({})
-    if (monitoresFaltantes.length < 2) {
-      return response.badRequest({ 'status': 400, 'mensaje': 'No hay suficientes monitores', 'error': [], 'data': [] })
-    }
+
   }
-   
-
-  
 
 
-  
+
+
+
+
 }
